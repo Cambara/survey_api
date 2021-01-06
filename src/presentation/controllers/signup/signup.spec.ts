d90d50576ee2f3600bec4b0efbc4d6065fcc3b5d
Tiago Cambará <tiago.cambara@gmail.com>
2021-01-06 21:42:00 +0000

    test: ensure SignUpController returns 200 when add a new user

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -151,4 +151,24 @@ describe('SignUp Controller', () => {
       password: 'any_pass'
     })
   })
+
+  test('Should return 200 if values are corrects', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    })
+  })
 })
